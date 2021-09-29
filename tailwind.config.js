8c5972a5bfac346b100218e146a3ba253e45dc8f
Petter Skogh <skoghpetter@gmail.com>
2021-09-29 15:14:43 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}','./components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      xlentyellow: {
+        light: 'rgb(255, 206, 122)',
+        DEFAULT: 'rgb(255, 170, 25)',
+        dark: 'rgb(168, 113, 19)',
+      }
+    },
+    fontFamily: {
+      'montserrat': ['Montserrat', 'Arial', 'sans-serif']
+    },
     extend: {},
   },
   variants: {
