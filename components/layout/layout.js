fa02c4dd3ad4a4de13b07ac2a60028e70fdea44d
Petter Skogh <skoghpetter@gmail.com>
2021-09-29 15:15:07 +0200

    Update layout.js
    Add font to layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -8,6 +8,7 @@ const Layout = (props) => {
         <title>Veckoaktivitet :)</title>
         <meta name="description" content="Veckans aktivitet handlar om React, Next.js och Tailwind" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
       </Head>
 
       {props.children}
