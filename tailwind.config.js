16c0d4e51094171e25bea1fed3ac5d6a21b87dbd
Petter Skogh <skoghpetter@gmail.com>
2021-09-30 10:23:06 +0200

    Update tailwind.config.js
    Update tailwind config to extend colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,18 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}','./components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      xlentyellow: {
-        light: 'rgb(255, 206, 122)',
-        DEFAULT: 'rgb(255, 170, 25)',
-        dark: 'rgb(168, 113, 19)',
-      }
+    extend: {
+      colors: {
+        xlentyellow: {
+          light: 'rgb(255, 206, 122)',
+          DEFAULT: 'rgb(255, 170, 25)',
+          dark: 'rgb(168, 113, 19)',
+        }
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'Arial', 'sans-serif']
+      },
     },
-    fontFamily: {
-      'montserrat': ['Montserrat', 'Arial', 'sans-serif']
-    },
-    extend: {},
   },
   variants: {
     extend: {},
